b0e55725ce4d1ae0adf45daa7ddf79e0bd7f0d37
Muhammad Fattah Ziidan <118587788+dann0204@users.noreply.github.com>
2024-12-01 20:34:04 +0700

    Add files via upload

---
+++ final-assessment/01-writing-comments/index.js
@@ -0,0 +1,9 @@
+// Username: mfz0204
+
+/*
+  Goal tahun ini:
+  1. Belajar JavaScript.
+  2. Menjadi Front-End atau Back-End Developer.
+*/
+
+
---
+++ final-assessment/optional-05-recursive/factorial.js
@@ -0,0 +1,5 @@
+function factorial(n) {
+    return n === 0 || n === 1 ? 1 : n * factorial(n - 1);
+}
+// Jangan hapus kode di bawah ini!
+export default factorial;
